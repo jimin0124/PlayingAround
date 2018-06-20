0d434bb258e96d021f06d2f11599f5f86931f3d3
GWC <gwc@lac-lab-12.local>
2018-06-20 11:11:54 -0700

    Initial Commit

---
+++ PlayingAround/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlayingAround
+//
+//  Created by GWC on 6/20/18.
+//  Copyright © 2018 Jimin Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
